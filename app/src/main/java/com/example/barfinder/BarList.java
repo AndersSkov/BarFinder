cc97d9340fc81f25f80b1beda9f70236924bdcf9
Anders <201804890@post.au.dk>
2020-01-28 13:52:39 +0100

    Activities er oppe

---
+++ app/src/main/java/com/example/barfinder/BarList.java
@@ -0,0 +1,14 @@
+package com.example.barfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BarList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bar_list);
+    }
+}
